2d641240cd446886075da6d9152bf8beacf17032
Iase <imamu2012@agruni.edu.ge>
2025-09-25 00:42:29 +0400

    Updated the UI - background and Author and Text fade in and fade out added

--- random-quote-generator/src/App.js
+++ random-quote-generator/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Quotebox from './components/Quotebox';
 import './App.scss';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
+
+  const [bgColor, setBgColor] = useState('#854e4e');
+
   return (
-    <Container className="d-flex justify-content-center align-items-center vh-100">
-      <Row xs={6} md={8} lg={20} className="justify-content-center">
-        <Col xs={8} md={50} lg={40} className="text-center">
-        <div className="App">
-       <Quotebox />
-       </div>
+    <Container 
+    fluid 
+    className="d-flex justify-content-center align-items-center vh-100"
+    style={{ backgroundColor: bgColor, transition: 'background-color 1s ease' }}>
+      <Row className="justify-content-center w-100">
+        <Col xs={12} sm={10} md={8} lg={6} xl={5} className="text-center h-100">
+          <div className="App">
+            <Quotebox setBgColor={setBgColor}/>
+          </div>
         </Col>
       </Row>
       
--- random-quote-generator/src/components/Quotebox.js
+++ random-quote-generator/src/components/Quotebox.js
@@ -1,21 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
 import './Quotebox.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-export default function Quotebox() {
+export default function Quotebox({ setBgColor }) {
   
   const [quote, setQuote] = useState( { text: "", author: "" } );
   const [loading, setLoading] = useState(false);
+  const [fade, setFade] = useState(false);
+
+  const colors = [
+    '#16a085', '#27ae60', '#2c3e50', '#f39c12', 
+    '#e74c3c', '#9b59b6', '#FB6964', '#342224', 
+    '#472E32', '#BDBB99', '#77B1A9', '#73A857'
+  ];
 
   const fetchQuote = async () => {
     setLoading(true);
+    setFade(true);
+
     try {
     const response = await fetch("https://quoteslate.vercel.app/api/quotes/random");
     const data = await response.json();
       console.log(data);
     if (data){
+      setTimeout(() => {
       setQuote( { text: data.quote, author: data.author } );
-      }
+      let randomColor = colors[Math.floor(Math.random() * colors.length)];
+      setBgColor(randomColor);
+      console.log(randomColor);
+      setFade(false);
+      }, 300);
+    }
   } catch (error) {
     console.error("Error fetching quote:", error);
   } finally {
@@ -32,14 +49,14 @@ export default function Quotebox() {
 
       <Card.Header className="card-header">Quote:</Card.Header>
 
-      <Card.Text id="text" className="p-4">
+      <Card.Text id="text" className={`p-4 ${fade ? 'fade-out' : 'fade-in'}`}>
         {loading ? "Loading..." : `"${quote.text}"`}
         </Card.Text>
 
-      <Card.Footer id="author" className="author-name">
+      <Card.Footer id="author" className={`author-name ${fade ? 'fade-out' : 'fade-in'}`}>
         {loading ? "Loading auth name..." : `- ${quote.author}`}
         </Card.Footer>
-        
+
       <div className="d-flex justify-content-between mt-auto">
 
           <a id="tweet-quote"
@@ -48,14 +65,19 @@ export default function Quotebox() {
           )}`}
           target="_blank"
           rel="noopener noreferrer"
-          className="btn btn-secondary  m-3"
+          className="btn btn-secondary m-3 btn-sm"
         >
-         Tweet Quote
+         <FontAwesomeIcon icon={faTwitter} />
        </a>
 
-        <Button id="new-quote" onClick={fetchQuote} disabled={loading} className="btn btn-primary m-3">
-        {loading ? "Loading..." : "New Quote"}
-          </Button>
+        <Button 
+          id="new-quote" 
+          onClick={fetchQuote} 
+          disabled={loading} 
+          className="btn btn-primary m-3 btn-sm"
+        >
+          {loading ? "Loading..." : "New Quote"}
+        </Button>
         
       </div>
 
