df2c27eca1e9217fd4e6f970e927041f13a3097b
Iase <imamu2012@agruni.edu.ge>
2025-09-24 08:29:43 +0400

    Feature implemented, functional

--- random-quote-generator/src/App.js
+++ random-quote-generator/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Quotebox from './components/Quotebox';
+import './App.scss';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <Quotebox />
-    </div>
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Row xs={6} md={8} lg={20} className="justify-content-center">
+        <Col xs={8} md={50} lg={40} className="text-center">
+        <div className="App">
+       <Quotebox />
+       </div>
+        </Col>
+      </Row>
+      
+    </Container>
   );
 }
 
--- random-quote-generator/src/components/Quotebox.js
+++ random-quote-generator/src/components/Quotebox.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
+import './Quotebox.scss';
 
 export default function Quotebox() {
   
@@ -29,31 +30,34 @@ export default function Quotebox() {
   return (
     <Card id="quote-box" className="text-center">
 
-      <Card.Header>Quote:</Card.Header>
+      <Card.Header className="card-header">Quote:</Card.Header>
 
       <Card.Text id="text" className="p-4">
         {loading ? "Loading..." : `"${quote.text}"`}
         </Card.Text>
 
-      <Card.Footer id="author" className="p-5">
+      <Card.Footer id="author" className="author-name">
         {loading ? "Loading auth name..." : `- ${quote.author}`}
         </Card.Footer>
-
-      <Button id="new-quote" onClick={fetchQuote} disabled={loading}>
-        {loading ? "Loading..." : "New Quote"}
-        </Button>
         
-      <a id="tweet-quote"
-        href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-          `"${quote.text}" - ${quote.author}`
-        )}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="btn btn-primary m-3"
-      >
-        Tweet Quote
-      </a>
+      <div className="d-flex justify-content-between mt-auto">
 
+          <a id="tweet-quote"
+         href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+           `"${quote.text}" - ${quote.author}`
+          )}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-secondary  m-3"
+        >
+         Tweet Quote
+       </a>
+
+        <Button id="new-quote" onClick={fetchQuote} disabled={loading} className="btn btn-primary m-3">
+        {loading ? "Loading..." : "New Quote"}
+          </Button>
+        
+      </div>
 
     </Card>
         );
